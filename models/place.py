a8dc94976e20e83d44c8e92970fd4a754073a51c
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-05-18 21:10:58 +0300

    ww

--- models/place.py
+++ models/place.py
@@ -1,86 +1,75 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-import models
+"""This is the place class"""
 from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Table, Column, Integer
-from sqlalchemy import Float, String, ForeignKey, MetaData
+from sqlalchemy import Column, String, Integer, Float, Table, ForeignKey
 from sqlalchemy.orm import relationship
+import models
+from os import environ as env
 
 
-if models.storage_type == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True,
-                                 nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True,
-                                 nullable=False)
-                          )
+place_amenity = Table(
+    'place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey("places.id")),
+    Column('amenity_id', String(60), ForeignKey("amenities.id"))
+)
 
 
 class Place(BaseModel, Base):
-    """ A place to stay """
-    if models.storage_type == "db":
-        __tablename__ = 'places'
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place",
-                               cascade="all, delete-orphan")
-        amenities = relationship("Amenity", secondary="place_amenity",
-                                 back_populates="place_amenities",
-                                 viewonly=False)
-    else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
+    """This is the class for Place
+    Attributes:
+        city_id: city id
+        user_id: user id
+        name: name input
+        description: string of description
+        number_rooms: number of room in int
+        number_bathrooms: number of bathrooms in int
+        max_guest: maximum guest in int
+        price_by_night:: pice for a staying in int
+        latitude: latitude in flaot
+        longitude: longitude in float
+        amenity_ids: list of Amenity ids
+    """
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
+    __reviews = relationship("Review", cascade="all, delete", backref="place")
+    __amenities = relationship(
+        "Amenity",
+        secondary=place_amenity,
+        backref="place",
+        viewonly=False
+    )
 
-        @property
-        def reviews(self):
-            """
-            Return the list of ``Review`` instances with ``place_id`` equal to
-            the current ``Place.id``
-            """
-            return [value for key, value in models.storage.all().items()
-                    if key.split(".")[0] == "Place"
-                    and value.place_id == self.id]
+    @property
+    def reviews(self):
+        """get all refiews with the current place id
+        from filestorage
+        """
+        if env.get('HBNB_TYPE_STORAGE') == 'db':
+            return self.__reviews
+        list = [
+            v for k, v in models.storage.all(models.Review).items()
+            if v.place_id == self.id
+        ]
+        return (list)
 
-        @property
-        def amenities(self):
-            """
-            Returns the list of ``Amenity`` instances with ``amenity_ids``
-            that contains all ``Amenity.id`` linked to the ``Place``
-            """
-            return [value for key, value in models.storage.all().items()
-                    if key.split(".")[0] == "Place"
-                    and value.place_id == self.id]
-
-        @amenities.setter
-        def amenities(self, amenity):
-            """
-            Setter attribute handles append method for adding ``Amenity.id``
-            to ``amenity_ids``. Only accepts ``Amenity`` objects.
-            """
-            from models.amenity import Amenity
-            if isinstance(amenity, Amenity):
-                self.amenity_ids.append(amenity.id)
+    @property
+    def amenities(self):
+        """get all amenities with the current place id
+        """
+        if env.get('HBNB_TYPE_STORAGE') == 'db':
+            return self.__amenities
+        list = [
+            v for k, v in models.storage.all(models.Amenity).items()
+            if v.id in self.amenity_ids
+        ]
+        return (list)
