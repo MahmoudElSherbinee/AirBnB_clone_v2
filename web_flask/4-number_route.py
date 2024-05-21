19eedf9525218f7d3231b7d689610df773fc60e5
MariaGeorge22 <maria.george2222@gmail.com>
2024-05-21 19:33:50 +0000

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,44 +1,42 @@
 #!/usr/bin/python3
-"""simple flask app
-"""
+"""First Module"""
+
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """root route
-    """
+def hello_world():
+    """rendered at path (/) """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """hbnb
-    """
+def hello_world_hbnb():
+    """rendered at path (/hbnb) """
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def cisfun(text):
-    """c what
-    """
-    return "C {}".format(text.replace('_', ' '))
-
-
-@app.route("/python/", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """python is cool
-    """
-    return "Python {}".format(text.replace('_', ' '))
+def hello_world_c_text(text):
+    """rendered at path (/c/<text>) """
+    return f"C {text.replace('_',' ')}"
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
-def intnumber(n):
-    """accept integer
-    """
-    return "{} is a number".format(n)
+def hello_world_number_n(n):
+    """rendered at path (/number/<n>) """
+    return f"{n} is a number"
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def hello_world_python_text(text):
+    """rendered at path (/python/<text>) """
+    return f"Python {text.replace('_',' ')}"
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    """main method"""
+    app.run(host="0.0.0.0", port=5000)
